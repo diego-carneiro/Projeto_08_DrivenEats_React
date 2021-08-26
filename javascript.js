371f3f289bc5a596ff27ee8f277daa4b28f7f94f
diego-carneio <diegosantos.cs@gmail.com>
2021-08-26 13:41:48 -0300

    First commit

---
+++ javascript.js
@@ -0,0 +1,182 @@
+function selectedfood1 () {
+    const marked1 = document.querySelector(".selected1-4");
+    
+    if (marked1 !== null) {
+
+        marked1.classList.remove("selected1-4");
+
+    }
+
+    const food1 = document.querySelector(".temaki");
+    food1.classList.add("selected1-4")
+    return marked1;
+
+}
+function selectedfood2 () {
+    
+    const marked1 = document.querySelector(".selected1-4");
+
+    if (marked1 !== null) {
+
+        marked1.classList.remove("selected1-4");
+
+    }
+    const food1 = document.querySelector(".macarrao");
+    food1.classList.add("selected1-4")
+    return marked1;
+
+}
+function selectedfood3 () {
+
+    const marked1 = document.querySelector(".selected1-4");
+    
+    if (marked1 !== null) {
+
+        marked1.classList.remove("selected1-4");
+
+    }
+    const food1 = document.querySelector(".feijoada");
+    food1.classList.add("selected1-4")
+    return marked1;
+
+}
+function selectedfood4 () {
+    
+    const marked1 = document.querySelector(".selected1-4");
+    
+    if (marked1 !== null) {
+
+        marked1.classList.remove("selected1-4");
+
+    }
+    const food1 = document.querySelector(".frango");
+    food1.classList.add("selected1-4")
+    return marked1;
+
+}
+function selectedfood5 () {
+    
+    const marked2 = document.querySelector(".selected5-8");
+    
+    if (marked2 !== null) {
+
+        marked2.classList.remove("selected5-8");
+
+    }
+    const food2 = document.querySelector(".refri");
+    food2.classList.add("selected5-8")
+    return marked2;
+
+}
+function selectedfood6 () {
+    
+    const marked2 = document.querySelector(".selected5-8");
+    
+    if (marked2 !== null) {
+
+        marked2.classList.remove("selected5-8");
+
+    }
+    const food2 = document.querySelector(".cerveja");
+    food2.classList.add("selected5-8")
+    return marked2;
+
+}
+function selectedfood7 () {
+    
+    const marked2 = document.querySelector(".selected5-8");
+    
+    if (marked2 !== null) {
+
+        marked2.classList.remove("selected5-8");
+
+    }
+    const food2 = document.querySelector(".agua");
+    food2.classList.add("selected5-8")
+    return marked2;
+
+}
+function selectedfood8 () {
+    
+    const marked2 = document.querySelector(".selected5-8");
+    
+    if (marked2 !== null) {
+
+        marked2.classList.remove("selected5-8");
+
+    }
+    const food2 = document.querySelector(".suco");
+    food2.classList.add("selected5-8")
+    return marked2;
+
+}
+function selectedfood9 () {
+    
+    const marked3 = document.querySelector(".selected9-12");
+    
+    if (marked3 !== null) {
+
+        marked3.classList.remove("selected9-12");
+
+    }
+    const food3 = document.querySelector(".petit");
+    food3.classList.add("selected9-12")
+    return marked3;
+
+}
+function selectedfood10 () {
+    
+    const marked3 = document.querySelector(".selected9-12");
+    
+    if (marked3 !== null) {
+
+        marked3.classList.remove("selected9-12");
+
+    }
+    const food3 = document.querySelector(".gelatina");
+    food3.classList.add("selected9-12")
+    return marked3;
+
+}
+function selectedfood11 () {
+    
+    const marked3 = document.querySelector(".selected9-12");
+    
+    if (marked3 !== null) {
+
+        marked3.classList.remove("selected9-12");
+
+    }
+    const food3 = document.querySelector(".mousse");
+    food3.classList.add("selected9-12");
+    return marked3;
+
+}
+function selectedfood12 () {
+    
+    const marked3 = document.querySelector(".selected9-12");
+    
+    if (marked3 !== null) {
+
+        marked3.classList.remove("selected9-12");
+
+    }
+    const food3 = document.querySelector(".trufa");
+    food3.classList.add("selected9-12");
+
+    return marked3;
+
+}
+
+function closeOrder(){
+
+
+
+}
+
+    
+
+
+
+
+
